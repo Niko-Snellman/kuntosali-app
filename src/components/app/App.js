5795d31a9a364f482e2d8ee450d808d6efd3404c
nikos <nikosnellman@hotmail.com>
2020-05-25 15:07:42 +0300

    tyylit lisätty navigointiin

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "../header/Header";
 import { Route, NavLink, BrowserRouter } from "react-router-dom";
@@ -6,13 +6,50 @@ import Diary from "../routes/diary/Diary";
 import Home from "../routes/home/Home";
 import Statistics from "../routes/statistics/Statistics";
 import Programs from "../routes/programs/Programs";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+} from "reactstrap";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="App">
       <Header />
+
       <BrowserRouter>
-        <ul>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">kuntosali app</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/ohjelmat">
+                  Ohjelma
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/tilastot">
+                  Tilastot
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/päiväkirja">
+                  Päiväkirja
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        {/*<ul>
           <li>
             <NavLink to="/" exact>
               Koti
@@ -27,7 +64,7 @@ function App() {
           <li>
             <NavLink to="/päiväkirja">Päiväkirja</NavLink>
           </li>
-        </ul>
+        </ul>*/}
 
         <Route path="/" exact strict component={Home}></Route>
         <Route path="/ohjelmat" component={Programs}></Route>
