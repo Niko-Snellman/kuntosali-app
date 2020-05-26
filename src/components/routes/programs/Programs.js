23b743ca86169abdb61812917014e069082fa986
nikos <nikosnellman@hotmail.com>
2020-05-26 15:05:51 +0300

    Update Programs.js
    Ptograms komponenttiin luotu nappi. Ja kovakoodatut kortit ohjelmia varten.

--- src/components/routes/programs/Programs.js
+++ src/components/routes/programs/Programs.js
@@ -1,9 +1,90 @@
 import React from "react";
+import {
+  Card,
+  CardTitle,
+  CardText,
+  Button,
+  Container,
+  Row,
+  Col,
+  CardBody,
+} from "reactstrap";
 
 const Programs = () => {
   return (
     <div>
-      <h2>Programs</h2>
+      <h2 className="m-4">Programs</h2>
+
+      <Container className="mt-4">
+        <Row>
+          <Col className="mb-3">
+            <Button>Luo uusi</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs="auto" className="mb-3">
+            <Card body outline style={{ width: "18rem" }}>
+              <CardBody>
+                <CardTitle>Kortti 1</CardTitle>
+                <CardText>
+                  Kortin teksti weugbvuweguywge w ug wetgywe gweu guwefg uwe
+                  fufev ewtfvwuefuwefuwefv
+                </CardText>
+                <Button>Nappi</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="auto" className="mb-3">
+            <Card body outline style={{ width: "18rem" }}>
+              <CardBody>
+                <CardTitle>Kortti 2</CardTitle>
+                <CardText>
+                  Kortin teksti weugbvuweguywge w ug wetgywe gweu guwefg uwe
+                  fufev ewtfvwuefuwefuwefv
+                </CardText>
+                <Button>Nappi</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="auto" className="mb-3">
+            <Card body outline style={{ width: "18rem" }}>
+              <CardBody>
+                <CardTitle>Kortti 3</CardTitle>
+                <CardText>
+                  Kortin teksti weugbvuweguywge w ug wetgywe gweu guwefg uwe
+                  fufev ewtfvwuefuwefuwefv
+                </CardText>
+                <Button>Nappi</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="auto" className="mb-3">
+            <Card body outline style={{ width: "18rem" }}>
+              <CardBody>
+                <CardTitle>Kortti 4</CardTitle>
+                <CardText>
+                  Kortin teksti weugbvuweguywge w ug wetgywe gweu guwefg uwe
+                  fufev ewtfvwuefuwefuwefv
+                </CardText>
+                <Button>Nappi</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="auto" className="mb-3">
+            <Card body outline style={{ width: "18rem" }}>
+              <CardBody>
+                <CardTitle>Kortti 5</CardTitle>
+                <CardText>
+                  Kortin teksti weugbvuweguywge w ug wetgywe gweu guwefg uwe
+                  fufev ewtfvwuefuwefuwefv
+                </CardText>
+                <Button>Nappi</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
