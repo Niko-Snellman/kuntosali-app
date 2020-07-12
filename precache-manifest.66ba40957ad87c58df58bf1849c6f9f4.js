15fe000c156546515d96085ef32c0d00716adbfd
nikos <nikosnellman@hotmail.com>
2020-07-12 22:33:22 +0300

    Updates

---
+++ precache-manifest.66ba40957ad87c58df58bf1849c6f9f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2455ac463ed472ea8a5d37ee3511b3a",
+    "url": "/kuntosali-app/index.html"
+  },
+  {
+    "revision": "aba589074211945d884d",
+    "url": "/kuntosali-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9573918f24766413dcaf",
+    "url": "/kuntosali-app/static/css/main.b7fac8f2.chunk.css"
+  },
+  {
+    "revision": "aba589074211945d884d",
+    "url": "/kuntosali-app/static/js/2.3be64961.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/kuntosali-app/static/js/2.3be64961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9573918f24766413dcaf",
+    "url": "/kuntosali-app/static/js/main.05512503.chunk.js"
+  },
+  {
+    "revision": "3808e2d73e4e2121933f",
+    "url": "/kuntosali-app/static/js/runtime-main.2785f81a.js"
+  }
+]);
